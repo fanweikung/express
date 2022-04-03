81466dec2e7043d0ba9e0d990202367e5bba4b03
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-04-03 04:29:36 -0700

    Add http routes and test with postman

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ import data from "./data/mock.json";
 const app = express();
 const PORT = 3000;
 
+// GET
+app.get("/", (request, response) => {
+  response.send("This is a GET request!");
+});
+
+// POST
+app.post("/create", (request, response) => {
+  response.send("This is a POST request at /create");
+});
+
+// PUT
+app.put("/edit", (request, response) => {
+  response.send("This is a PUT request at /edit");
+});
+
+// DELETE
+app.delete("/delete", (request, response) => {
+  response.send("This is a DELETE request at /delete");
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running at port ${PORT}!`);
   console.log(data);
